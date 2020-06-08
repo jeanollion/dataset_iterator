93153400e89cb679b3d74aecf5e52a458da9b2e2
jean.ollion <jean.ollion@polytechnique.org>
2020-06-08 10:48:55 +0200

    added simple image generator for preprocessing

--- dataset_iterator/__init__.py
+++ dataset_iterator/__init__.py
@@ -5,10 +5,10 @@ from .multichannel_iterator import MultiChannelIterator
 from .tracking_iterator import TrackingIterator
 from .dy_iterator import DyIterator
 from .delta_iterator import DeltaIterator
-from .autoencoder_iterator import AutoencoderIterator
 from .tile_utils import extract_tiles, augment_tiles, extract_tile_function, augment_tiles_inplace
 from .multiple_fileIO import MultipleFileIO
 from .datasetIO import DatasetIO
 from .multiple_fileIO import MultipleFileIO
 from .multiple_datasetIO import MultipleDatasetIO
 from .concatenate_datasetIO import ConcatenateDatasetIO
+from .preprocessing_image_generator import PreProcessingImageGenerator
