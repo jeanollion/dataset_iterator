- index_array_iterator.py added get_sample_number, get_batch_size, set_index_probability methods - helpers: modified get_histogram bin computation - improved hard_sample_mining.py callback (fixed memory leak) - added publish workflow

dataset_iterator/concat_iterator.py
@@ -1,3 +1,4 @@
+from math import isclose
 import numpy as np
 from .index_array_iterator import IndexArrayIterator, INCOMPLETE_LAST_BATCH_MODE
 from .utils import ensure_multiplicity, ensure_size
@@ -12,7 +13,23 @@ class ConcatIterator(IndexArrayIterator):
                  incomplete_last_batch_mode:str=INCOMPLETE_LAST_BATCH_MODE[1],
                  step_number:int=0):
         assert isinstance(iterators, (list, tuple)), "iterators must be either list or tuple"
-        self.iterators = iterators
+        self.iterators = []
+        def append_it(iterator): # unroll concat iterators
+            if isinstance(iterator, (list, tuple)):
+                for subit in iterator:
+                    append_it(subit)
+            elif isinstance(iterator, ConcatIterator):
+                for subit in iterator.iterators:
+                    append_it(subit)
+            else:
+                self.iterators.append(iterator)
+
+        append_it(iterators)
+        bs = [it.get_batch_size() for it in self.iterators]
+        assert np.all(np.array(bs) == bs[0] ), "all sub iterator batch_size must be equal"
+        for it in self.iterators:
+            it.incomplete_last_batch_mode = incomplete_last_batch_mode
+        self.sub_iterator_batch_size = bs[0]
         if proportion is None:
             proportion = [1.]
         self.proportion = ensure_multiplicity(len(iterators), proportion)
@@ -28,6 +45,12 @@ class ConcatIterator(IndexArrayIterator):
         if choice and self.index_probability is not None:
             array = np.random.choice(array, size=array.shape[0], replace=True, p=self.index_probability)
         return array
+
+    def get_sample_number(self):
+        return self.it_cumlen[-1]
+
+    def get_batch_size(self):
+        return self.batch_size * self.sub_iterator_batch_size
 
     def _set_index_array(self):
         indices_per_iterator = []
@@ -56,8 +79,8 @@ class ConcatIterator(IndexArrayIterator):
 
     def _get_batches_of_transformed_samples(self, index_array):
         index_array = np.copy(index_array) # so that main index array is not modified
-        index_it = self._get_it_idx(index_array) # modifies index_array
-
+        index_it = self._get_it_idx(index_array) # modifies index_array so that indices are relative to each iterator
+        #batches = [self.iterators[it_idx]._get_batches_of_transformed_samples(index_array[index_it==it_idx]) for it_idx in np.unique(index_it)]
         batches = [self.iterators[it][i] for i, it in zip(index_array, index_it)]
         for i in range(1, len(batches)):
             assert len(batches[i])==len(batches[0]), f"Iterators have different outputs: batch from iterator {index_it[0]} has length {len(batches[0])} whereas batch from iterator {index_it[i]} has length {batches[i]}"
@@ -65,7 +88,7 @@ class ConcatIterator(IndexArrayIterator):
         if len(batches[0]) == 2:
             inputs = [b[0] for b in batches]
             outputs = [b[1] for b in batches]
-            return (concat_numpy_arrays(inputs), concat_numpy_arrays(outputs))
+            return concat_numpy_arrays(inputs), concat_numpy_arrays(outputs)
         else:
             return concat_numpy_arrays(batches)
 
@@ -94,7 +117,16 @@ class ConcatIterator(IndexArrayIterator):
     def enable_random_transforms(self, parameters):
         for it, params in zip(self.iterators, parameters):
             it.enable_random_transforms(params)
-            
+
+    def set_index_probability(self, value): # set to sub_iterators/ expects a concatenated vector in the order of sub iterators
+        cur_idx = 0
+        for it in self.iterators:
+            size = it.get_sample_number()
+            proba = value[cur_idx:cur_idx+size]
+            it.index_probability = proba / np.sum(proba)
+            cur_idx+=size
+        assert cur_idx == value.shape[0], f"invalid index_probability length expected: {cur_idx} actual {value.shape[0]}"
+
 def concat_numpy_arrays(arrays):
     if isinstance(arrays[0], (list, tuple)):
         n = len(arrays[0])

dataset_iterator/helpers.py
@@ -102,13 +102,18 @@ def get_histogram(dataset, channel_keyword:str, bins, bin_size=None, sum_to_one:
     if bins is None:
         assert bin_size is not None
         vmin, vmax = get_min_and_max(dataset, channel_keyword, batch_size=batch_size)
-        n_bins = round( (vmax - vmin) / bin_size )
-        bin_size = (vmax - vmin) / n_bins
-        bins = np.linspace(vmin, vmax, num=n_bins+1)
-    if isinstance(bins, int):
+        n_bins = int( 1 + (vmax - vmin ) / bin_size )
+        bin_size = (vmax - vmin ) / (n_bins - 1)
+        bins = np.linspace(vmin, vmax + bin_size, num=n_bins+1)
+        #print(f"range: [{vmin}; {vmax}] nbins: {n_bins} binsize: {bin_size} bins: {bins}")
+    elif isinstance(bins, int):
+        assert bins>1, "at least 2 bins"
         vmin, vmax = get_min_and_max(dataset, channel_keyword, batch_size=batch_size)
-        bin_size = (vmax - vmin)/bins
-        bins = np.linspace(vmin, vmax, num=bins+1)
+        bin_size = (vmax - vmin)/(bins-1)
+        bins = np.linspace(vmin, vmax + bin_size, num=bins+1)
+    else:
+        assert isinstance(bins, (list, tuple))
+        vmin = bins[0]
     histogram = None
     for i in range(len(iterator)):
         batch = iterator[i]

dataset_iterator/index_array_iterator.py
@@ -36,6 +36,15 @@ class IndexArrayIterator(tf.keras.preprocessing.image.Iterator):
             array = np.copy(array)
         return array
 
+    def get_sample_number(self):
+        return len(self.allowed_indexes)
+
+    def get_batch_size(self):
+        return self.batch_size
+
+    def set_index_probability(self, value):
+        self.index_probability = value
+
     def __len__(self):
         if self.step_number > 0:
             return self.step_number
@@ -123,6 +132,7 @@ class IndexArrayIterator(tf.keras.preprocessing.image.Iterator):
     @index_probability.setter
     def index_probability(self, value):
         if value is not None:
-            assert len(self.allowed_indexes) == len(value), f"invalid index_probability length: expected: {len(self.allowed_indexes)} actual {len(value)}"
+            size = self.get_sample_number()
+            assert size == len(value), f"invalid index_probability length: expected: {size} actual {len(value)}"
             assert isclose(np.sum(value), 1.), "probabilities do not sum to 1"
         self._index_probability = value