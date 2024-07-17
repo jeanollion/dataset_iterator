212130d3d46cb31b310cc6a76e0645f8d972c59d
jeanollion <smogollion@yahoo.fr>
2024-07-17 12:12:16 +0200

    v0.4.8

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dataset_iterator",
-    version="0.4.7",
+    version="0.4.8",
     author="Jean Ollion",
     author_email="jean.ollion@polytechnique.org",
     description="Keras-style data iterator for images contained in dataset files such as hdf5 or PIL readable files. Images can be contained in several files.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/jeanollion/dataset_iterator.git",
-    download_url='https://github.com/jeanollion/dataset_iterator/releases/download/v0.4.7/dataset_iterator-0.4.7.tar.gz',
+    download_url='https://github.com/jeanollion/dataset_iterator/releases/download/v0.4.8/dataset_iterator-0.4.8.tar.gz',
     keywords=['Iterator', 'Dataset', 'Image', 'Numpy'],
     packages=setuptools.find_packages(),
     classifiers=[ #https://pypi.org/classifiers/
