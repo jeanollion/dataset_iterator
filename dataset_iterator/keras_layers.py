0339effba963327a87f2df5b284b83744dd275b0
jeanollion <smogollion@yahoo.fr>
2026-02-26 17:41:22 +0100

    remove identity layer

--- dataset_iterator/keras_layers.py
+++ dataset_iterator/keras_layers.py
@@ -5,12 +5,3 @@ class InferenceLayer:
         self.inference_mode = False
         super().__init__(*args, **kwargs)
 
-# for compat with tf2.7
-class Identity(tf.keras.layers.Layer):
-    def __init__(self, autocast=False, **kwargs):
-        super().__init__(autocast=autocast **kwargs)
-
-    def call(self, input, training=None):
-        if not training:
-            return input
-        return tf.identity( input, name=self.name )
